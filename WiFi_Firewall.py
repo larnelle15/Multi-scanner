bb9cc37cb3e164a9e891d47d71730aa3030ff5ee
Larnelle Ankunda <larnelleankunda50@gmail.com>
2023-12-22 17:34:44 -0500

    Update WiFi_Firewall.py

--- WiFi_Firewall.py
+++ WiFi_Firewall.py
@@ -1,7 +1,7 @@
 import os
 import socket
 
-
+#Checking the Ip adresses of incoming traffic
 def check_ip():
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.bind (("", 0))
@@ -13,13 +13,16 @@ def check_ip():
     return address[0]
   except socket.timeout:
     return None
-
+    
+#creating a dictionary to store the IP addresses that I want to block
 blocked_ips = {}
 
+#block traffic from specific addresses
 def block_ip(ip_address):
   os.system("iptables -A INPUT -s " + ip_address + " -j DROP")
   blocked_ips[ip_address] = True
-
+  
+# check for incoming traffic from specific IP addreses
 while True:
   ip_address = check_ip()
   if ip_address is not None and ip_address not in blocked_ips:
